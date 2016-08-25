f1b8cea1a7d90d0aa3a35dbcf0de4c4582e49869
manish8856 <manish.kumar@crichq.com>
2016-08-25 15:24:21 +0530

    update

---
+++ mysql/recipes/server.rb
@@ -0,0 +1,32 @@
+::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
+::Chef::Recipe.send(:include, Opscode::Mysql::Helpers)
+
+if Chef::Config[:solo]
+  missing_attrs = %w[
+    server_debian_password
+    server_root_password
+    server_repl_password
+  ].select { |attr| node['mysql'][attr].nil? }.map { |attr| %Q{node['mysql']['#{attr}']} }
+
+  unless missing_attrs.empty?
+    Chef::Application.fatal! "You must set #{missing_attrs.join(', ')} in chef-solo mode." \
+    " For more information, see https://github.com/opscode-cookbooks/mysql#chef-solo-note"
+  end
+else
+  # generate all passwords
+  node.set_unless['mysql']['server_debian_password'] = secure_password
+  node.set_unless['mysql']['server_root_password']   = secure_password
+  node.set_unless['mysql']['server_repl_password']   = secure_password
+  node.save
+end
+
+case node['platform_family']
+when 'rhel'
+  include_recipe 'mysql::_server_rhel'
+when 'debian'
+  include_recipe 'mysql::_server_debian'
+when 'mac_os_x'
+  include_recipe 'mysql::_server_mac_os_x'
+when 'windows'
+  include_recipe 'mysql::_server_windows'
+end
