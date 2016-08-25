d59d86adc30908359c365d28f882f894d32b8e5e
manish8856 <manish.kumar@crichq.com>
2016-08-25 14:22:40 +0530

    update

--- mysql/metadata.rb
+++ mysql/metadata.rb
@@ -1,7 +1,1 @@
-name        "mysql"
-description "Installs and configures MySQL"
-maintainer  "AWS OpsWorks"
-license     "Apache 2.0"
-version     "5.5"
-
-depends "opsworks_commons"
+depends 'mysql', '~> 8.0'
--- mysql/recipes/default.rb
+++ mysql/recipes/default.rb
@@ -1,11 +1,11 @@
-mysql_service "default" do
+mysql_client "default" do
   port "3306"
   version "5.5"
   initial_root_password "manish"
   action [:create, :start]
 end
 
-mysql_config "default" do
+mysql_service "default" do
   source "mysite.cnf.erb"
   notifies :restart, "mysql_service[default]"
   action :create
