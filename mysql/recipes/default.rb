d770fc8c98e73a5c9fb46351c58ca3ecd6d3ef83
manish8856 <manish.kumar@crichq.com>
2016-08-25 11:32:41 +0530

    update

--- mysql/recipes/default.rb
+++ mysql/recipes/default.rb
@@ -1,4 +1,12 @@
-package 'mysql-server' do
-  version node['mysql']['version']
-  action :install
+mysql_service 'default' do
+  port '3306'
+  version '5.5'
+  initial_root_password 'manish'
+  action [:create, :start]
+end
+
+mysql_config 'default' do
+  source 'mysite.cnf.erb'
+  notifies :restart, 'mysql_service[default]'
+  action :create
 end
