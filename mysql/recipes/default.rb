c3dcda25297009bb8017e2e24ed12811562e26e2
manish8856 <manish.kumar@crichq.com>
2016-08-25 15:06:30 +0530

    update

--- mysql/recipes/default.rb
+++ mysql/recipes/default.rb
@@ -1,12 +1,42 @@
-mysql_client "default" do
-  port "3306"
-  version "5.5"
-  initial_root_password "manish"
-  action [:create, :start]
+#----
+# Set up preseeding data for debian packages
+#---
+directory '/var/cache/local/preseeding' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
 end
 
-mysql_service "default" do
-  source "mysite.cnf.erb"
-  notifies :restart, "mysql_service[default]"
-  action :create
+template '/var/cache/local/preseeding/mysql-server.seed' do
+  source 'mysql-server.seed.erb'
+  owner 'root'
+  group 'root'
+  mode '0600'
+  notifies :run, 'execute[preseed mysql-server]', :immediately
 end
+
+execute 'preseed mysql-server' do
+  command '/usr/bin/debconf-set-selections /var/cache/local/preseeding/mysql-server.seed'
+  action  :nothing
+end
+
+#----
+# Install software
+#----
+node['mysql']['server']['packages'].each do |name|
+  package name do
+    action :install
+  end
+end
+
+node['mysql']['server']['directories'].each do |key, value|
+  directory value do
+    owner     'mysql'
+    group     'mysql'
+    mode      '0775'
+    action    :create
+    recursive true
+  end
+end
+
